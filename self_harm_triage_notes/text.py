a4439262e7c1c13784bb0e6f914cda797a6c97b3
vlada rozova <vsrozova@gmail.com>
2025-02-04 18:53:46 +1100

    Add print_token_counts

--- self_harm_triage_notes/text.py
+++ self_harm_triage_notes/text.py
@@ -5,3 +5,8 @@ def count_tokens(x):
     tokens = []
     x.apply(lambda y: [tokens.append(token) for token in y.split()])
     return Counter(tokens)
+
+def print_token_counts(x):
+    """Print stats for token counts."""
+    counts = count_tokens(x)
+    print("The corpus contains %d unique tokens (%d tokens in total)." % (len(counts), sum(counts.values())))
