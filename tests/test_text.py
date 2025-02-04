10d673206414197625369e3a009a13dded25cf45
vlada rozova <vsrozova@gmail.com>
2025-02-05 09:25:01 +1100

    Unit test for fix_leading_fullstop()

--- tests/test_text.py
+++ tests/test_text.py
@@ -6,9 +6,9 @@ def test_count_tokens():
     """Test values and type returned by count_tokens"""
     x = pd.Series(['I have a tasty apple', 
                    'A red apple is tasty.'])
-    res = count_tokens(x)
     exp_res = Counter({'I': 1, 'have': 1, 'a': 1, 'tasty': 1, 'apple': 2, 
                        'A': 1, 'red': 1, 'is': 1, 'tasty.': 1})
+    res = count_tokens(x)
     # Check that type is Counter
     assert type(res)==Counter
     # Check that values are correct
@@ -19,3 +19,14 @@ def test_print_token_counts():
     x = pd.Series(['I have a tasty apple', 
                    'A red apple is tasty.'])
     print_token_counts(x)
+
+def test_fix_leading_fullstop():
+    """Test text is processed correctly by fix_leading_fullstop"""
+    text = "i have an apple .the apple is tasty."
+    exp_res = "i have an apple . the apple is tasty."
+    res = fix_leading_fullstop(text)
+    # Check that type is str
+    assert type(res)==str
+    # Check that text is processed correctly
+    assert res==exp_res
+
