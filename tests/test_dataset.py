ff396652d6c838706c30031afa10f10a8a319f0e
vlada rozova <vsrozova@gmail.com>
2025-02-04 13:34:37 +1100

    Test for print_stats

---
+++ tests/test_dataset.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from self_harm_triage_notes.dataset import *
+
+def test_print_stats():
+    """Execution test for print_stats."""
+    df = pd.DataFrame({
+        'SH': [1,0,0,1],
+        'SI': [1,1,1,0],
+        'AOD': [0,0,1,0],
+    })
+    print_stats(df)
