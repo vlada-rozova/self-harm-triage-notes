c696dedf6081c841b9bebfc3e94d234e5761a450
vlada rozova <vsrozova@gmail.com>
2025-02-05 09:38:31 +1100

    Unit tests for preprocess() and is_valid_token()

--- tests/test_text.py
+++ tests/test_text.py
@@ -3,7 +3,7 @@ import pandas as pd
 from collections import Counter
 
 def test_count_tokens():
-    """Test values and type returned by count_tokens"""
+    """Test values and type returned by count_tokens()."""
     x = pd.Series(['I have a tasty apple', 
                    'A red apple is tasty.'])
     exp_res = Counter({'I': 1, 'have': 1, 'a': 1, 'tasty': 1, 'apple': 2, 
@@ -15,13 +15,13 @@ def test_count_tokens():
     assert res==exp_res
 
 def test_print_token_counts():
-    """Execution test for print_token_counts."""
+    """Execution test for print_token_counts()."""
     x = pd.Series(['I have a tasty apple', 
                    'A red apple is tasty.'])
     print_token_counts(x)
 
 def test_fix_leading_fullstop():
-    """Test text is processed correctly by fix_leading_fullstop"""
+    """Test text is processed correctly by fix_leading_fullstop()."""
     text = "i have an apple .the apple is tasty."
     exp_res = "i have an apple . the apple is tasty."
     res = fix_leading_fullstop(text)
@@ -29,4 +29,49 @@ def test_fix_leading_fullstop():
     assert type(res)==str
     # Check that text is processed correctly
     assert res==exp_res
+
+def test_preprocess():
+    """Execution test for preprocess()."""
+    text = "Pt presents with fever, t37.8."
+    preprocess(text)
+    ### Need example-based tests for every pre-processing step!!!
+
+def test_is_valid_token():
+    """Test values and type returned by is_valid_token()"""
+    # Example 1
+    token = "patient"
+    exp_res = True
+    res = is_valid_token(token)
+    # Check that type is bool
+    assert type(res)==bool
+    # Check that value is correct
+    assert res==exp_res
+
+    # Example 2
+    token = "t2dm"
+    exp_res = True
+    res = is_valid_token(token)
+    # Check that type is bool
+    assert type(res)==bool
+    # Check that value is correct
+    assert res==exp_res
+
+    # Example 3
+    token = "37.8"
+    exp_res = False
+    res = is_valid_token(token)
+    # Check that type is bool
+    assert type(res)==bool
+    # Check that value is correct
+    assert res==exp_res
+
+    # Example 4
+    token = "&"
+    exp_res = False
+    res = is_valid_token(token)
+    # Check that type is bool
+    assert type(res)==bool
+    # Check that value is correct
+    assert res==exp_res
+
 
