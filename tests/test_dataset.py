68fcd926c96d7da58d41392868c395eeb90ed667
vlada rozova <vsrozova@gmail.com>
2025-02-04 13:56:48 +1100

    Fix test_print_stats()

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -6,6 +6,6 @@ def test_print_stats():
     df = pd.DataFrame({
         'SH': [1,0,0,1],
         'SI': [1,1,1,0],
-        'AOD': [0,0,1,0],
+        'AOD_OD': [0,0,1,0],
     })
     print_stats(df)
