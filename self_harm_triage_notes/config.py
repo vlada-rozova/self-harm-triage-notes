48734923b38f72de3f7400f59aef87ceb0ab4307
vlada rozova <vsrozova@gmail.com>
2025-02-07 17:55:44 +1100

    Add paths to resources

--- self_harm_triage_notes/config.py
+++ self_harm_triage_notes/config.py
@@ -13,3 +13,18 @@ data_pred_dir = root / "data/predicted"
 
 # Data to support spelling correction
 spell_corr_dir = root / "data/spelling correction"
+
+# Resources directory
+resources_dir = root.parents[2] / "Resources"
+
+# Dictionary of English words
+scowl_dir = resources_dir / "scowl-2020.12.07/final"
+
+# Paths to AMT reference lists
+amt_ed_path = resources_dir / "AMT/Australian-emergency-department-reference-set-20230831-expansion.tsv"
+amt_mp_path = resources_dir / "AMT/Medicinal-product-reference-set-20230831-expansion.tsv"
+amt_tp_path = resources_dir / "AMT/Trade-product-reference-set-20230831-expansion.tsv"
+
+# Path to the list of abbreviations
+abbr_path = resources_dir / "Abbreviations/Clinical Abbreviations List - SESLHDPR 282 cleaned.xlsx"
+
