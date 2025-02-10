552b5e847b8426de20dbc32a182fbd61a7272291
vlada rozova <vsrozova@gmail.com>
2025-02-10 14:36:35 +1100

    Func to load slang dictioanry

--- self_harm_triage_notes/dev.py
+++ self_harm_triage_notes/dev.py
@@ -1,6 +1,7 @@
-from self_harm_triage_notes.config import spell_corr_dir
+from self_harm_triage_notes.config import spell_corr_dir, resources_dir
 import json
 from collections import Counter
+import pandas as pd
 
 
 def load_vocab(filename):
@@ -36,3 +37,21 @@ def load_misspelled_dict(filename):
     print("Spelling correction available for %d words." % len(misspelled_dict))
         
     return misspelled_dict
+
+def load_slang_dict():
+    """
+    Create a dictionary of slang used for medications mapped to their generic names.
+    """
+    # Load medication names
+    drugs = pd.read_csv(resources_dir / "medication_names.csv")
+
+    drugs.slang = drugs.slang.str.strip().str.lower()
+    drugs.generic_name = drugs.generic_name.str.strip().str.lower()
+    drugs.dropna(subset=["slang"], inplace=True)
+
+    # Create a dictionary to convert slang to generic names
+    slang_dict = dict(zip(drugs.slang, drugs.generic_name))
+
+    print("Slang available for %d words." % len(slang_dict))
+
+    return slang_dict
