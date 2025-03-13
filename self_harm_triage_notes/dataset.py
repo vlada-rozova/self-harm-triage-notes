1206310ec327604e444d61a9cc5c0abcdf8900c9
vlada rozova <vsrozova@gmail.com>
2025-03-14 10:53:30 +1100

    Sort output in print_stats()

--- self_harm_triage_notes/dataset.py
+++ self_harm_triage_notes/dataset.py
@@ -1,28 +1,28 @@
 def print_stats(df):
     """
     Print the number of presentations and the proportion of self-harm, suicidal ideation and AOD OD cases.
-    v1 from 13.12.23
+    v2.sh from 18.12.23
     """
     print("The dataset contains %d presentations.\n" % df.shape[0])
     print("SELF-HARM")
     print("Number of presentations:")
-    print(df.SH.value_counts(dropna=False))
+    print(df.SH.value_counts(dropna=False).sort_index())
     print()
     print("Proportion of presentations:")
-    print(df.SH.value_counts(dropna=False, normalize=True)*100)
+    print(df.SH.value_counts(dropna=False, normalize=True).sort_index()*100)
     print()
     print("_"*80)
     print("SUICIDAL IDEATION")
     print("Number of presentations:")
-    print(df.SI.value_counts(dropna=False))
+    print(df.SI.value_counts(dropna=False).sort_index())
     print()
     print("Proportion of presentations:")
-    print(df.SI.value_counts(dropna=False, normalize=True)*100)
+    print(df.SI.value_counts(dropna=False, normalize=True).sort_index()*100)
     print("_"*80)
     print("AOD overdose")
     print("Number of presentations:")
-    print(df.AOD_OD.value_counts(dropna=False))
+    print(df.AOD_OD.value_counts(dropna=False).sort_index())
     print()
     print("Proportion of presentations:")
-    print(df.AOD_OD.value_counts(dropna=False, normalize=True)*100)
+    print(df.AOD_OD.value_counts(dropna=False, normalize=True).sort_index()*100)
     print()  
